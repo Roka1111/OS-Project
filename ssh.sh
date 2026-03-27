24c442d5a82bcdfe22eafaddb8d4e775032e9788
Roka1111 <rokamr542@gmail.com>
2026-03-27 15:33:07 +0100

    Create ssh.sh to generate and send system report
    This script generates a system report including CPU, RAM, and disk usage, and sends it to a specified user via SCP.

---
+++ ssh.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+echo "===== SYSTEM REPORT =====" > report.txt
+echo "Date: $(date)"  >> report.txt
+echo "Hostname: $(hostname)" >> report.txt
+echo "User: $(whoami)" >> report.txt
+
+echo "----- CPU -----" >> report.txt
+top -b -n1 | head -5 >> report.txt
+echo "----- RAM -----" >> report.txt
+free -h  >> report.txt
+
+echo "----- DISK -----"  >> report.txt
+df -h  >> report.txt
+
+echo "----- LAST LOGINS -----" >> report.txt
+last -n 3 >> report.txt
+
+
+echo "Enter the username :"
+read user
+echo "Enter the IP :"
+read ip
+echo "sending report ..."
+scp report.txt $user@$ip:~/reports/ 2>/dev/null  #the recipient should have this directory
+echo "Done"
